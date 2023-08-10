fbc416a08806436569e4898b95933c95a14a11a2
Alaa <lolomuhammd21@gmail.com>
2023-08-10 10:31:48 -0400

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,23 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
 #include <stdio.h>
 
-int main()
+/**
+ * main - Entry point
+ * 
+ * Description: ' a C program that prints the size of various types on the computer it is compiled and run on'
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-    puts("Hello World");
+    puts("\"Programming is like building a multilingual puzzle");
 
     return 0;
 }
