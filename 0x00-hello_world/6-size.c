963f63b21732170fea3b7149237b048f6df4ad36
Alaa <lolomuhammd21@gmail.com>
2023-08-10 09:53:47 -0400

    from 4>>6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,12 @@
 
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * 
+ * Description: ' a C program that prints the size of various types on the computer it is compiled and run on'
+ * 
+ * Return: Always 0 (Success)
+ */
 int main(void) {
   printf("Type:char,Storage size is:%zu \n", sizeof(char));
  printf("Type:unsigned char,Storage size is:%zu \n",sizeof(unsigned char ));
