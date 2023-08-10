72a659e03c79fb1840216ff0691fb00d2af79a15
Alaa <lolomuhammd21@gmail.com>
2023-08-10 09:46:42 -0400

    from 4>>6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,15 @@
 
 #include <stdio.h>
 
-int main() {
- printf (" Type:char,Storage size is:%zu \n", sizeof(char)) ;
- printf ("Type:unsigned char,Storage size is:%zu \n",sizeof(unsigned char )) ;
- printf ("Type:short,Storage size is:%zu \n", sizeof (short)) ;
- printf("Type:unsigned short,Storage size is:%zu \n",sizeof (unsigned short)) ;
+int main(void) {
+  printf("Type:char,Storage size is:%zu \n", sizeof(char));
+ printf("Type:unsigned char,Storage size is:%zu \n",sizeof(unsigned char ));
+ printf("Type:short,Storage size is:%zu \n", sizeof (short));
+ printf("Type:unsigned short,Storage size is:%zu \n",sizeof (unsigned short));
  printf("Type:int,Storage size is:%zu \n", sizeof(int));
- printf ("Type:unsigned int,Storage size is:%zu \n", sizeof (unsigned int)) ;
- printf ("Type:long,Storage size is:%zu \n", sizeof (long)) ;
- printf ("Type:unsigned long,Storage size is:%zu \n",sizeof (unsigned long)) ;
+ printf("Type:unsigned int,Storage size is:%zu \n", sizeof (unsigned int));
+ printf("Type:long,Storage size is:%zu \n", sizeof (long));
+ printf("Type:unsigned long,Storage size is:%zu \n",sizeof (unsigned long));
  
     return 0;
 }
