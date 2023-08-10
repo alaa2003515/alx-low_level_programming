e96693f8c37d3eac93f47272ed6c9d515b11419c
Alaa <lolomuhammd21@gmail.com>
2023-08-10 10:37:11 -0400

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,6 @@
 /**
  * main - Entry point
  * 
- * Description: ' a C program that prints the size of various types on the computer it is compiled and run on'
- * 
  * Return: Always 0 (Success)
  */
 int main(void)
