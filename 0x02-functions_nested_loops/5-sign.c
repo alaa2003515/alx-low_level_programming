96ec2838d8f9c1750153210c8683294ad8cffd95
Alaa <lolomuhammd21@gmail.com>
2023-08-15 15:47:24 -0400

    5. Sign 

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,16 +14,20 @@ int print_sign(int n)
 
 if (n > 0)
 {
+putchar ('+');
 return (1);
-_putchar ('+');
+
 }
 else if (n == 0)
 {
+
+putchar ('0');
 return (0);
-_putchar ('0');
 }
 else
 {
+putchar ('-');
 return (-1);
-_putchar ('-');
+
 }
+
