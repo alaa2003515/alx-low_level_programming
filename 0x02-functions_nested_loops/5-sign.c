5bf729ead1c33190f547a6d9c635e5a5e24789b9
Alaa <lolomuhammd21@gmail.com>
2023-08-15 15:32:48 -0400

    5. Sign 

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *print_sign - function that checks for lowercase character
+ *@n: number to check
+ * Description :function that checks for lowercase character
+ * Return: Always 0 (Success)
+ */
+
+
+int print_sign(int n)
+{
+
+
+if (n > 0)
+{
+return (1);
+putchar ('+');
+}
+else if (n == 0)
+{
+return (0);
+putchar ('0');
+}
+else
+{
+return (-1);
+putchar ('-');
+}
+
+
+}
