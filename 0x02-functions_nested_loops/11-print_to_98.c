a93b3e9d28434172efc58b72a266dcb499834e31
Alaa <lolomuhammd21@gmail.com>
2023-08-15 19:00:56 -0400

    6. There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you 

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_to_98 - a function that prints all natural numbers from n to 98
+ *@n: number to check
+ * Description : prints all natural numbersi
+* Return: zero
+ *
+ */
+
+
+void print_to_98(int n)
+{
+
+     
+for (n=0; n <= 98 ; n++)
+{
+
+printf ("%d",n);
+printf(", ");
+
+}
+
+for (n=-98; n < 0 ; n++)
+{
+
+printf ("%d",n);
+printf(", ");
+}
+
+
+
+}
+
+
