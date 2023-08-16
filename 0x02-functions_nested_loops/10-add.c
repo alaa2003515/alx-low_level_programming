eebcc96a6283bf46336c375db12f900be817af50
Alaa <lolomuhammd21@gmail.com>
2023-08-16 07:48:49 -0400

    10. a + b 

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  add - returns the result of adds two integers
+ * Description : a function that adds two integers and returns the result
+ * @wr: number 1
+ * @tr: number to 2
+ * Return: Always the result (Success)
+ */
+
+
+int add(int tr, int wr)
+{
+
+int oop = wr + tr;
+return (opp);
+
+}
