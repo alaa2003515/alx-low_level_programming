4c4e07f11a00c4b47b88e1c4551bd552cdb299dd
Alaa <lolomuhammd21@gmail.com>
2023-08-25 19:43:56 -0400

    ALL 

---
+++ 0x06-pointers_arrays_strings/0-reset_to_98.c~
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_strcat - a function that concatenates two strings.
+ *@d: arry 2 ____
+ *@s: arry 1__
+ * Return: always (d)
+ */
+
+char *_strcat(char *d, char *s)
+{
+int n;
+int x = 0;
+int y = 0;
+for (x = 0 ; d[x++]; x++)
+
+while (x = 0)
+{
+(s[x] && x < n)
+x++;
+d [y++] = s[x];
+}
+return (d);
+}
---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *_strcat - aa function that concatenates two strings.
+ *@d: arry____
+ *@s:arry of num_ber________
+ * Return: returns always (0) ______
+ */
+
+char *_strcat(char *d, char *s)
+{
+int siko = 0
+int miko = 0;
+do {
+siko++;
+} while (d[siko]);
+
+while (s[miko])
+{
+d[siko++] = s[miko];
+miko++;
+}
+}
