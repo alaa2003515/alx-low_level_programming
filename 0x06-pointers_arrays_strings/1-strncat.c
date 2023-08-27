5a4b1b28d8f31ff94d563322cf086e6f85d72584
Alaa <lolomuhammd21@gmail.com>
2023-08-27 17:07:43 -0400

    1 

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,7 +4,7 @@
  *_strncat - A function that concatenates two stringS.
  *@dj: arry____
  *@sj:arry of num_ber________
- *@nj:number to check _____
+ *@nj:num_ber to check___
  * Return: returns always (0) ______
  */
 
@@ -13,18 +13,15 @@ char *_strncat(char *dj, char *sj, int nj)
 int lmn_1 = 0;
 int lnm_2 = 0;
 
-while (*(dj + lmn_1) != '\0')
+while (dj[lmn_1] != '\0')
 {
 lmn_1++;
 }
-
-
 do {
-*(dest +  lmn_1) = *(sj + lmn_2);
-if (*(sj + lmn_2) == '\0')
-break;
+d[lmn_1] = s[lmn_2];
 lmn_1++;
 lmn_2++;
-} while (lmn_2 < nj);
+} while (lmn_2 < nj && sj[lmn_2] != '\0');
+dj[lmn_1] = '\0';
 return (dj);
 }
