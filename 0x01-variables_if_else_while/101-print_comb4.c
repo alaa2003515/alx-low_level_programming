c4b1f015e9bf5fdfdbf43c2e25e48d71519f9ef9
Alaa <lolomuhammd21@gmail.com>
2023-08-27 09:53:14 -0400

    101 

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *Discrbation :A program that prints all different combinations of two digits
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+int x = 0;
+int y = 0;
+int z = 0;
+for (y = 0; y < 8; y++)
+{
+for (x = y + 1; x < 9; x++)
+{
+for (z = x + 1; z < 10; z++)
+{
+putchar ('0' + y);
+putchar ('0' + x);
+putchar ('0' + z);
+
+if (y != 7 || x != 8 || z != 9)
+{
+putchar (',');
+putchar (' ');
+}
+}
+}
+}
+putchar ('\n');
+
+return (0);
+
+}
