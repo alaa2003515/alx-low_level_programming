63ea1ffba8245cb04b0778939740eabaf90b0111
Alaa <lolomuhammd21@gmail.com>
2023-08-27 16:11:20 -0400

    0-strcat.c  

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 int _putchar(char c);
-char *_strcat(char *dest, char *src);
+char *_strcat(char *d, char *s);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
