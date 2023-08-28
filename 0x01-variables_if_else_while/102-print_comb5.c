133ba564cdc095a9282bbbf246b711387e3eea4a
Alaa <lolomuhammd21@gmail.com>
2023-08-28 14:02:14 -0400

    12 

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,16 +10,16 @@
 
 int main(void)
 {
-int x = 0;
-int y = 0;
-for (y = 0; y < 100; y++)
+int xoo = 0;
+int yoo = 0;
+for (yoo = 0; yoo < 99; yoo++)
 {
-for (x = y; x < 100; x++)
+for (xoo = yoo + 1; xoo < 100; xoo++)
 {
-int dig_1 = (y / 10) % 10;
-int dig_2 = y % 10;
-int dig_3 = (x / 10) % 10;
-int dig_4 = x % 10;
+int dig_1 = (yoo / 10) % 10;
+int dig_2 = yoo % 10;
+int dig_3 = (xoo / 10) % 10;
+int dig_4 = xoo % 10;
 
 putchar('0' + dig_1);
 putchar('0' + dig_2);
@@ -27,11 +27,10 @@ putchar(' ');
 putchar('0' + dig_3);
 putchar('0' + dig_4);
 
-if (y != 99 || x != 99)
-{
+if (yoo == 98 && xoo == 99)
+continue;
 putchar(',');
 putchar(' ');
-}
 }
 }
 putchar ('\n');
