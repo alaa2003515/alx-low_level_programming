d2607c800c713e57f751738edfe37965af9262ba
Alaa <lolomuhammd21@gmail.com>
2023-08-28 11:30:48 -0400

    11 

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+/**
+ * main - a program and prints the largest prime factor of the number
+ * Return: A_l_w_a_y_s (0).
+ */
+
+int main(void)
+{
+long alaa;
+long int lol = 612852475143;
+long lolo;
+double lo = sqrt(lol);
+
+for (alaa = 1; alaa <= lo; alaa++)
+{
+if (lol % alaa == 0)
+{
+lolo = lol / alaa;
+}
+}
+printf("%ld\n", lolo);
+return (0);
+}
