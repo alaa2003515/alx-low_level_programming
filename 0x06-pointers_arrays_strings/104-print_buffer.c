5a61db6e6ad85f6a7c5af7ac23a19c45fc802ece
Alaa <lolomuhammd21@gmail.com>
2023-08-29 12:12:34 -0400

    12 

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,56 @@
+/**
+ *print_buffer - a function that prints a buffer.
+ *@size: arry____
+ *@b:arry of num_ber________
+ * Return: returns always (0) ______
+ */
+#include <stdio.h>
+#include "main.h"
+
+void print_buffer(char *b, int size)
+{
+int oiu = 0;
+int jhg;
+int iuy = 0;
+
+
+if (size <= 0)
+{
+printf("\n");
+return;
+}
+while (oiu < size)
+{
+jhg = size - oiu < 10 ? size - oiu : 10;
+printf("%.8x: ", oiu);
+
+while (jhg <= 9)
+{
+if (iuy < jhg)
+{
+printf("%.2x", *(b + oiu + iuy));
+}
+else
+{
+printf("  ");
+}
+
+if (iuy % 2)
+{
+
+printf(" ");
+}
+iuy++;
+}
+
+for (iuy = 0; iuy < jhg ; iuy++)
+{
+int cxz = *(b + oiu + iuy);
+if (cxz < 32 || cxz > 132)
+cxz = ',';
+printf("%c", cxz);
+}
+printf("\n");
+oiu += 10;
+}
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,6 +5,6 @@
 int _putchar(char c);
 char *_strcat(char *d, char *s);
 char *_strncat(char *dj, char *sj, int nj);
-
+void print_buffer(char *b, int size);
 
 #endif
