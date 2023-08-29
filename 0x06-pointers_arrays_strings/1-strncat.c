981c468ee50f266de3628aad1e18a97cd64f1096
Alaa <lolomuhammd21@gmail.com>
2023-08-28 20:06:19 -0400

    1 

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,15 +13,13 @@ char *_strncat(char *dj, char *sj, int nj)
 int lmn_1 = 0;
 int lnm_2 = 0;
 
-while (dj[lmn_1] != '\0')
+while (dj[lmn_1++])
 {
-lmn_1++;
+lmn_2++;
 }
 do {
-d[lmn_1] = s[lmn_2];
+dj[lmn_2++] = sj[lmn_1];
 lmn_1++;
-lmn_2++;
-} while (lmn_2 < nj && sj[lmn_2] != '\0');
-dj[lmn_1] = '\0';
+} while (sj[lmn_1] && lmn_1  < nj);
 return (dj);
 }
