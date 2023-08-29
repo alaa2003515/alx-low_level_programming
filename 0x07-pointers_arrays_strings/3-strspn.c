bb268495151d20bdb5c7e3f0d28301b153e41de9
Alaa <lolomuhammd21@gmail.com>
2023-08-29 18:06:19 -0400

    3 

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strspn-a  function that gets the length of a prefix substring.
+ *@s:arry of num_ber________
+ *@accept:arry 1_______
+ * Return: returns always (0) ______
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int lolo = 0;
+unsigned int body = 0;
+unsigned int mody = 0;
+unsigned int walaa = 0;
+
+
+for (; *(s + i) != '\0'; lolo++)
+{
+for (; *(accept + body) != '\0'; body++)
+{
+if (*(accept + body) == *(s + lolo))
+{
+mody++
+walaa = 1;
+}
+}
+}
+}
+
