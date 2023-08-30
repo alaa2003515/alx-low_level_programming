4f510116df7f6a01c04cdaadcc951c4d9a737aef
Alaa <lolomuhammd21@gmail.com>
2023-08-29 20:05:42 -0400

    7 not 8 

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_diagsums-a function that prints the sum of the two diagonals.
+ *@a:arry of num_ber________
+ *@size:arry 1_______
+ * Return: returns always (0) ______
+ */
+
+void print_diagsums(int *a, int size)
+{
+int io = 0;
+int soso_1 = 0;
+int soso_2 = 0;
+int nono;
+
+for (io = 0; i <= (size * size); io += size + 1)
+soso_1 += *(a + io);
+for (n = size - 1; nono <= (size * size) - size; nono += size - 1)
+soso_2 += *(a + nono);
+printf("%d, %d\n", soso_1, soso_2);
+}
