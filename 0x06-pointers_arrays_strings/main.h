78df2448f60ac8b6bd1fa53d34ee84447f39f947
Alaa <lolomuhammd21@gmail.com>
2023-08-29 21:02:21 -0400

    2 

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,7 +5,7 @@
 int _putchar(char c);
 char *_strcat(char *d, char *s);
 char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n);
 void print_buffer(char *b, int size);
 
 #endif
