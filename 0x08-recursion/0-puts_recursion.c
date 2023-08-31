09ceeffb5b0ece7cff519b09118f7bbd80ea08d2
Alaa <lolomuhammd21@gmail.com>
2023-08-31 10:58:44 -0400

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget 

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,13 +7,16 @@
  */
 void _puts_recursion(char *s)
 {
-if ((*s >= '0') && (*s <= '\0'))
+if ((*s >= '0') && (*s < '\0'))
 {
 
 _putchar(*s);
 _putchar('\n');
 }
-else
-return;
 
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
 }
