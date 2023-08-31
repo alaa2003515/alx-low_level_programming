d1b4a71d7a7f9ef7f82c44be5642a3001ac55ca6
Alaa <lolomuhammd21@gmail.com>
2023-08-31 18:56:35 -0400

    3. You mustn't be afraid to dream a little bigger, darling 

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_pow_recursion-function that returns the value of x raised to the power of y.
+ *@y: numberof power ___
+ *@x: number to start___
+ * Return:factorial (lolo).
+ */
+int _pow_recursion(int x, int y)
+
+{
+
+if (y < 0)
+{
+return (-1);
+}
+else if (y == 0)
+{
+return (1);
+}
+else
+{
+return (x);
+}
+
+return (x * _pow_recursion(int x, int y));
+
+
+
+}
