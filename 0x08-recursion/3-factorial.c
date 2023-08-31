20f6607be49b186ff7ffd70087cd59d679061a14
Alaa <lolomuhammd21@gmail.com>
2023-08-31 15:21:33 -0400

    3. You mustn't be afraid to dream a little bigger, darling 

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *_strlen_recursion-a function that returns the factorial of a given number.
+ *factorial-a function that returns the factorial of a given number.
  *@lolo: number to start___
  * Return:factorial (lolo).
  */
 int factorial(int lolo)
 
 {
-if (lolo == 0)
+if ((lolo == 0) || (lolo == 1))
 return (1);
 return (lolo * factorial(lolo - 1));
 }
