90805027c919d64717420fb3a559f3a49954b83d
Alaa <lolomuhammd21@gmail.com>
2023-09-01 22:36:58 -0400

    4. To infinity and beyond 

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - a program that multiplies two numbers.
+ * @argc: number of command line arguments.
+ * @argv: An array of size argc______
+ * Return: always (0)_____
+ */
+
+
+int main(int argc, char *argv[])
+{
+int no_1 = 0;
+int no_2 = 0;
+if (argc == 3)
+{
+no_1 = atoi(argv[1]);
+no_2 = atoi(argv[1]);
+printf("%d", no_1 *no_2);
+putchar("\n");
+}
+else
+}
+putchar("Error");
+putchar('\n');
+return (1);
+}
