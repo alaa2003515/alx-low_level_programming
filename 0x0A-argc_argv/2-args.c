5b1ab24c8dabf6f07527084f1caa9385dd84892f
Alaa <lolomuhammd21@gmail.com>
2023-09-01 22:04:33 -0400

    4. To infinity and beyond 

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - a program that prints all arguments it receives..
+ * @argc: number of command line arguments.
+ * @argv: An array of size argc______
+ * Return: always (0)_____
+ */
+
+
+int main(int argc, char *argv[])
+{
+int no;
+for (no = 0; no < argc; no++ )
+{
+printf("%s\n", argv[no]);
+}
+return (0);
+}
