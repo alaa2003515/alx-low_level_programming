4b724e7f76272b6786b9b74ac8334c27973dc8d7
Alaa <lolomuhammd21@gmail.com>
2023-09-04 17:58:35 -0400

    0x0B. C - malloc, free 

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ *_strdup -a function  returns a pointer to a newly allocated space in memory
+ *@str:arry 1_______
+ * Return: returns always (0) ______
+ */
+char *_strdup(char *str)
+{
+unsigned int tall;
+char *KOKA;
+int i;
+if (str == 0 || KOKA == 0)
+{
+return (NULL);
+}
+for (tall = 0; str[tall] != 0; tall++)
+KOKA = malloc((tall + 1) * sizeof(char));
+for (i = 0; i <= tall; i++)
+{Koka[i] = str[i];
+
+return (Koka);
+}
+}
