314f5f5dad465d31fff551af27a743ab6a70443a
Alaa <lolomuhammd21@gmail.com>
2023-09-04 16:13:02 -0400

    0x0B. C - malloc, free 

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ **create_array- creates an array of chars and initializes with a specific char
+ *@c: numbers to start____
+ *@size:size _______
+ * Return: returns always (NULL) or (KOKA) ______
+ */
+char *create_array(unsigned int size, char c);
+{
+
+int num;
+char *KOKA;
+KOKA = malloc(1 * sizeof(char));
+if (size == 0 || KOKA == 0)
+{
+return (NULL);
+}
+for (num = 0; num < size; num++)
+{
+KOKA[num] = c;
+return (KOKA);
+}
+}
