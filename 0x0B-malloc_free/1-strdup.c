39227d76ff04b50c17a647b633b4d2347bd9aaca
Alaa <lolomuhammd21@gmail.com>
2023-09-05 12:05:14 -0400

    0x0B. C - malloc, free 

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,12 +18,12 @@ return (NULL);
 }
 for (tall = 0; str[tall] != 0; tall++)
 KOKA = malloc(tall * sizeof(char) + 1);
-if (Koka == 0)
+if (KOKA == 0)
 return (NULL);
 else
 {
 for (i = 0; i < tall; i++)
-return (Koka);
+return (KOKA);
 }
 
 }
