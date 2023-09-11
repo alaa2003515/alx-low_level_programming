d98abd41822ecb8dc4ad78426b162bb27edd2251
Alaa <lolomuhammd21@gmail.com>
2023-09-11 19:04:03 -0400

    struct 

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog-Write a struct dog
+ *@owner:owner of dog___________
+ *@age:age of Doges________
+ *@name:naMes of Doges_______________
+ * Return: returns always (0) ______
+ */
+struct dog
+{
+
+char *name;
+float age;
+char *owner;
+
+};
+
+#endif
