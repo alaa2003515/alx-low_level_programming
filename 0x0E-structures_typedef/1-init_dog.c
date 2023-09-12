6e7197050b9db01a2e8d2e41f81c26a95dd12bc1
Alaa <lolomuhammd21@gmail.com>
2023-09-11 18:26:16 -0700

    S

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,7 +2,7 @@
 #include "dog.h"
 
 /**
- *init_dog- a function that initialize a variable of type struct dog
+ *init_dog - a function that initialize a variable of type struct dog
  *@owner:owner of dog___________
  *@age:age of Doges________
  *@name:naMes of Doges_______________
