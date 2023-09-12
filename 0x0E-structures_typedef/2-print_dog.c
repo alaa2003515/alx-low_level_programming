2a5009746f40e1c642c4a9bca97bdf80a79cf62f
Alaa <lolomuhammd21@gmail.com>
2023-09-12 18:25:44 -0400

    structures 

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,18 +14,18 @@ if (d)
 
 if (d->name)
 {
-printf("Name: %s\n", d->name);
-printf("Age: %f\n", d->age);
+
+printf("Name: (nil)\n");
 }
 else
 {
-printf("Name: (nil)\n");
+printf("Name: %s\n", d->name);
+printf("Age: %f\n", d->age);
 }
 if (d->owner)
-printf("Owner: %s\n", d->owner);
-else
 printf("Owner: (nil)\n");
-
+else
+printf("Owner: %s\n", d->owner);
 }
 }
 
