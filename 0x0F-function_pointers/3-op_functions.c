1e6b1f99db5cadce1e9421718c0cbda5cf84589c
Alaa <lolomuhammd21@gmail.com>
2023-09-13 19:16:15 -0400

    function_pointers 

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - a function returns the sum of two numbers_______
+ * @a: the frist number____
+ * @b: the second number ____
+ * Return: returns always (a + b) ______
+ */
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ *op_sub - a function returns the difference of two numbers_______
+ * @a: the frist number____
+ * @b: the second number ____
+ * Return: returns always (a - b) ______
+ */
+
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul- a function returns the product of two numbers_______
+ * @a: the frist number____
+ * @b: the second number ____
+ * Return: returns always (a * b) ______
+ */
+
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - a function returns the result of the division of two numbers__
+ * @a: the frist number____
+ * @b: the second number ____
+ * Return: returns always (a / b) ______
+ */
+
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * op_mod - a function returns the result of the remainder of the division
+ * @a: the frist number____
+ * @b: the second number ____
+ * Return: returns always (a % b) ______
+ */
+
+int op_mod(int a, int b)
+{
+return (a % b);
+}
