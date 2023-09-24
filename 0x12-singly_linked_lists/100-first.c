5393513c12dccff420fc3e1713a729115454d01c
Alaa <lolomuhammd21@gmail.com>
2023-09-24 18:44:57 -0400

    0x12-singly_linked_lists 

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,15 +3,15 @@
 void __constructor(void) __attribute__ ((constructor));
 
 /**
- * _constructor - a function that prints_______
+ * __constructor - a function that prints_______
  *
  * Return: no Return.
  */
 
 
-void _constructor(void)
+void __constructor(void)
 {
-printf("You're beat! and yet, you must allow,\n"
-"I bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 fflush(stdout);
 }
