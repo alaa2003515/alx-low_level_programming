e240ef9c40e19c11ffde69d00d4389e415c86ee4
Alaa <lolomuhammd21@gmail.com>
2023-09-25 10:59:42 -0400

    0x0C-more_malloc_free 

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * mim -a function help ___
+ *@num: var 1_____
+ *@b: var 3 ____
+ *@so: var 2 _____
+ * Return: return no_2 ____
+ **/
+
+char *mim(char *so, char b, unsigned int num)
+{
+
+char *pointer = so;
+while (num--)
+*so++ = b;
+return (pointer);
+}
+
+
+/**
+ *_calloc-a function allocates memory for an array using malloc___
+ *@nmemb: var 1_____
+ *@size: var 2 _____
+ * Return: return no_2 ____
+ **/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void *mk;
+if ((size == 0) || (nmemb == 0))
+return (NULL);
+mk = malloc(sizeof(int) * nmemb);
+if (mk == NULL)
+return (NULL);
+mim(mk, 0, sizeof(int) * nmemb);
+return (mk);
+}
