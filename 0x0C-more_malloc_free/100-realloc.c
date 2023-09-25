8b3dc6bb19dafa9d90dd15e63c665b37d712b116
Alaa <lolomuhammd21@gmail.com>
2023-09-25 11:53:08 -0400

    0x0C-more_malloc_free 

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/**
+ *_realloc-a function that reallocates a memory block using malloc and free___
+ *@ptr: var 1_____
+ *@old_size: var 2 _____
+ * @new_size: var 3 ______
+ * Return: return no_2 ____
+ **/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+void *mk;
+unsigned int ip;
+
+if (new_size ==  old_size)
+return (mk);
+if (new_size == 0 && !mk)
+{
+free(mk);
+return (NULL);
+}
+if (mk == NULL)
+{
+mk = malloc(new_size);
+if (mk == NULL)
+return (NULL);
+return (mk);
+}
+}
