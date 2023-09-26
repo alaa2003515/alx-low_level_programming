574b77801f8e697cc0cea5b9e534d3d3104a1a18
Alaa <lolomuhammd21@gmail.com>
2023-09-26 19:53:45 -0400

    0x13-more_singly_linked_lists 

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -22,38 +22,3 @@ return (totall->next);
 }
 return (NULL);
 }
-/**
- *free_1 -a program that multiplies two positive numbers_
- * @h: pointer_____
- * Return: no return___
- */
-size_t free_1(listint_t **h)
-{
-listint_t *next;
-listint_t *node;
-int loo = 1;
-size_t lolo;
-if ((h == NULL) || (*h == NULL))
-return (0);
-node = findout(*h);
-for (lolo = 0; (*h != node || loo) && *h != NULL; *h = next)
-{
-lolo++;
-next = (*h)->next;
-if (*h == node && loo)
-{
-if (node == node->next)
-{
-free(*h);
-break;
-}
-lolo++;
-next = next->next;
-free((*h)->next);
-loo = 0;
-}
-free(*h);
-}
-*h = NULL;
-return (lolo);
-}
