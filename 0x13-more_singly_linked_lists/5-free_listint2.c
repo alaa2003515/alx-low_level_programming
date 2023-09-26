1f1a581cbc4f4837bdf10d210a877ae4975b799a
Alaa <lolomuhammd21@gmail.com>
2023-09-26 11:58:01 -0400

    0x13-more_singly_linked_lists 

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ *free_listint2 - a function that frees a listint_t list____
+ * @head: pointer_____
+ * Return: no return___
+ */
+
+void free_listint2(listint_t **head)
+{
+listint_t *n_o_d_e;
+listint_t *t_a_l;
+if (head != NULL)
+return;
+n_o_d_e = *head;
+while (n_o_d_e)
+{
+t_a_l = n_o_d_e;
+n_o_d_e = n_o_d_e->next;
+free(t_a_l);
+}
+*head = NULL;
+}
---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ *pop_listint-a function deletes head node of a listint_t linked list____
+ * @head: pointer_____
+ * Return:returns the head node’s data (nayama)___
+ */
+
+int pop_listint(listint_t **head)
+{
+listint_t *n_o_d_e;
+int n;
+if (!head || !*head)
+return (0);
+n_o_d_e = (*head)->next;
+n = (*head)->n;
+free(head);
+*head = n_o_d_e;
+return (n);
+}
