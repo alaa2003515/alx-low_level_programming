13abc8230bac96c729892376f0b70add3a3c9436
Alaa <lolomuhammd21@gmail.com>
2023-09-29 20:40:17 -0400

    0x14-bit_manipulation 

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,11 +12,17 @@ unsigned int binary_to_uint(const char *b)
 {
 unsigned int soso = 0;
 if (!b)
+return (0);
 while (*b)
 {
 if ((*b != '0') && (*b != '1'))
+{
 return (0);
+
+}
 soso = soso * 2 + (*b++ - '0');
+
+
 }
 return (soso);
 }
