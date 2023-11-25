5569fcbfaeb29674194cffa3be8774094b0c6e8f
Alaa <lolomuhammd21@gmail.com>
2023-11-25 03:11:46 -0500

    0x02-functions_nested_loops 

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,42 +1,50 @@
-#include <stdio.h>
+#include"main.h"
 
 /**
- * _times_table - a function that prints the n times table, starting with 0.
+ * print_times_table - function that prints the n times table, starting with 0
  *
- * Return: Always___ (0)
- */
+ * @n :function input
+*/
+
 void print_times_table(int n)
 {
-int nji;
-int mko;
-int pl;
+	int x, y, prod;
 
-if (n >= 0 && n < 16)
-{
-for (nji = 0; nji <= n; nji++)
-{
-putchar('0');
-for (mko = 1; mko <= n; mko++)
-{
-putchar(',');
-putchar(' ');
-pl = nji * mko;
-if (pl < 100 )
-putchar(' ');
-if (pl < 10)
-putchar(' ');
-if (pl >= 100)
-{
-putchar((pl / 100) + '0');
-putchar(((pl / 10)) % 10 + '0');
-}
-else if (pl < 100 && pl >= 10)
-{
-putchar((pl / 10) + '0');
-}
-putchar((pl % 10) + '0');
-}
-putchar('\n');
-}
-}
+	if (n >= 0 && n <= 15)
+	{
+		for (x = 0; x <= n; x++)
+		{
+			_putchar(48);
+			for (y = 1; y <= n; y++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				prod = x * y;
+				if (prod < 10)
+					_putchar(' ');
+				if (prod < 100)
+				{
+					_putchar(' ');
+				}
+				if (prod > 100)
+				{
+					_putchar((prod / 100) + 48);
+					_putchar(((prod / 10) % 10) + 48);
+				}
+				if (prod == 100)
+				{
+					_putchar((prod / 100) + 48);
+					_putchar(48);
+				}
+				else if (prod >= 10 && prod <= 99)
+				{
+					_putchar ((prod / 10) + 48);
+
+				}
+				_putchar((prod % 10) + 48);
+
+			}
+			_putchar('\n');
+		}
+	}
 }
