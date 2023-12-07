0aca9017d17a54677507b37a6b69efa78f60a8fe
Alaa <lolomuhammd21@gmail.com>
2023-12-07 11:37:30 -0500

    0x17

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -8,23 +8,29 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	unsigned int ip = 0;
-	dlistint_t *tmp;
+	dlistint_t *tmp = *head;
 
-	tmp = *head;
-	while ((tmp->next != NULL) && (ip < index))
+if (*head == NULL)
+
+return (-1);
+	for (; index != 0; index--)
 	{
+		if (tmp == NULL)
+		return (-1);
 		tmp = tmp->next;
-		ip++;
 	}
-	if (tmp)
-	{
-		if (tmp->prev)
+if (tmp == *head)
+{
+*head = tmp->next;
+if (*head != NULL)
+(*head)->prev = NULL;
+}
+else
+{
 			tmp->prev->next = tmp->next;
-		if (tmp->next)
+		if (tmp->next != NULL)
 			tmp->next->prev = tmp->prev;
-		free(tmp);
-		return (1);
 	}
-	return (-1);
+	free(tmp);
+	return (1);
 }
